bc948ac453bff0c80cc97cc7ab6712468a327adf
Rohit Sharma <rohit.sharma2k2k@gmail.com>
2025-06-25 11:42:55 +0530

    Update Holdings.js

--- dashboard/src/Components/Holdings.js
+++ dashboard/src/Components/Holdings.js
@@ -15,7 +15,7 @@ const Holdings = () => {
   const pnlPercentage = ((totalPnL / totalInvestment) * 100).toFixed(2);
 
   useEffect(() => {
-    axios.get('https://zerodha2-2.onrender.com/allHoldings')
+    axios.get('https://zerodha2-1.onrender.com/allHoldings')
       .then((res) => {
         setAllHoldings(res.data)
         console.log(res.data)
