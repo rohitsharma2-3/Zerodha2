78ded8f5412917092847f26f735fdfaa96f76e8e
Rohit Sharma <rohit.sharma2k2k@gmail.com>
2025-07-01 23:19:07 +0530

    Update Education.js

--- frontend/src/landing-page/Home/Education.js
+++ frontend/src/landing-page/Home/Education.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Education = () => {
   return (
-    <div className='container p-lg-5'>
+    <div className='container p-5'>
       <div className='row'>
         <div className='col-12 col-lg-6'>
           <img src='media\Images\education.svg' alt='ed-img' style={{width: '100%'}}/>
